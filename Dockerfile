dc9394f41b27d3f0a0243d166a5aa5d6693aabf4
h4nyu <yao.ntno@gmail.com>
2019-12-16 08:02:03 +0000

    ddd

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM rust:1-slim
+FROM rust:1.39-slim
+
 
 WORKDIR /srv
 COPY . .
--- src/models.rs
+++ src/models.rs
@@ -1,27 +1,4 @@
+
 pub struct Point {
     pub value: f64,
 }
-
-pub struct PointLike {
-    pub value: f64,
-}
-
-pub struct Human {
-    pub age: u8,
-}
-
-pub fn func0(p:&Point) -> f64 {
-    p.value
-}
-
-pub trait Mammals {
-    fn do_mammals_work(& self);
-}
-
-
-impl Mammals for Human {
-    fn do_mammals_work(& self) {
-        println!("人間は哺乳類です。");
-    }
-}
-
